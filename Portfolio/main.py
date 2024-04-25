0b70b7e9f3652100607baf98aa7e30a79b879ba2
Apurva12S <apurva.srivastava.1812@gmail.com>
2024-04-25 22:11:58 +0530

    created colums, changed my picture

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -1,1 +1,19 @@
 import streamlit as st 
+
+st.set_page_config(layout="wide")
+col1 , col2 =  st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg" )
+
+with col2:
+    st.title("Apurva Srivastava")
+    content = """ Hi, I am Apurva. I am a LLM App Developer 
+    I am a bachelor of technology in computer science, 
+    after which I pursued MBA to hone my leadership skills.
+    I am a dedicated professional with a strong work ethic,
+    quick learner, and enthusiastic team player.
+    Committed to delivering top-notch results 
+    through effective communication, problem-solving, 
+    and meticulous attention to detail in dynamic environments. """
+    st.info(content)
