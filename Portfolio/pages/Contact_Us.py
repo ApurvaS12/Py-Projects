00871dc22512ab864d94318a12d521246939db1c
Apurva12S <apurva.srivastava.1812@gmail.com>
2024-04-26 03:08:03 +0530

    added selection box

--- Portfolio/pages/Contact_Us.py
+++ Portfolio/pages/Contact_Us.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from send_email import send_email 
+import pandas as pd
+
+options_df = pd.read_csv("topics.csv")
+
+options = options_df["topic"].tolist()
 
 st.title("Contact Me")
 
@@ -7,11 +12,13 @@ st.title("Contact Me")
 
 with st.form(key="contact"):
     user_email = st.text_input("**Your Email Address**")
+    option = st.selectbox("Select an option" , options)
     raw_message = st.text_area("**Your message**")
     message =  f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic : {option}
 {raw_message}
 """
     button =  st.form_submit_button("Submit")
