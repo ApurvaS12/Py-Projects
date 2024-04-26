53fd3cef9627fc1dac0ef6f6d46851a2abda4426
Apurva12S <apurva.srivastava.1812@gmail.com>
2024-04-26 12:53:19 +0530

    lined the document

--- PDF_Generator/main.py
+++ PDF_Generator/main.py
@@ -1,29 +1,39 @@
-from fpdf import FPDF as fd
+from fpdf import FPDF
 import pandas as pd
 
-pdf = fd(orientation="P", unit="mm", format="A4")
-pdf.set_auto_page_break(auto=False,margin=0)
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
-
 for index, row in df.iterrows():
     pdf.add_page()
+
+    # Set the header
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100,100,100)
-    pdf.cell(w=0, h=12, txt= row["Topic"], align="L", ln=1 )
-    pdf.line(10,21,200,21)
-    pages = int(row["Pages"])
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
+         ln=1)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
-    while pages>1:
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
-        pdf.ln(270)
+
+        # Set the footer
+        pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
-        pdf.set_text_color(180,180,180)
-        pdf.cell(w=0,h=20,txt=row["Topic"], align="R")
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-        pages = pages-1
-    
-
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
