5925055c6e5ee0379148007b2eb066fe31f6e6dc
Apurva12S <apurva.srivastava.1812@gmail.com>
2024-04-30 10:02:15 +0530

    add links, add subject, control category

--- Daily Email News Digest/main.py
+++ Daily Email News Digest/main.py
@@ -3,9 +3,10 @@ from send_email import send_email
 
 api_key="47fa2572b9004ba88f7835c66fb2f8c8"
 
-
-url = ('https://newsapi.org/v2/top-headlines?'
-       'country=in&'
+topic = "houthis"
+url = ('https://newsapi.org/v2/everything?'
+       f"q={topic}&"
+       'sortBy=publishedAt&'
        'language=en&'
        'apiKey=47fa2572b9004ba88f7835c66fb2f8c8')
 #make request
@@ -14,15 +15,16 @@ request =  requests.get(url)
 #dictionory of response
 content =  request.json()
 
-message = ""
+message = "Subject: Today's news" + "\n"
 count = 1
 #access the acrticle titles and description
-for article in content["articles"]:
+for article in content["articles"][:20]:
     title = article["title"]
     description = article["description"]
-    message = message + f"News {count}:" \
-        +"\n" +title +"\n" + description +"\n"
-    count+=1
+    message = message +"\n" +title.title() +"\n" \
+        + description + "\n" +article["url"]\
+        +"\n" + "\n"
+
     
     
 message = message.encode("utf-8")
--- Daily Email News Digest/send_email.py
+++ Daily Email News Digest/send_email.py
@@ -14,13 +14,16 @@ def send_email(message):
     with open("password.txt" , "r") as file:
         password = file.readline()
 
-    receiver = "llmappdev@gmail.com"
+    receiver1= "llmappdev@gmail.com"
+ 
 
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host,port,context=context) as server:
         server.login(username, password)
-        server.sendmail(username,receiver,message)
+        server.sendmail(username,receiver1,message)
+     #  server.sendmail(username,receiver2,message)
+
 
 
 
