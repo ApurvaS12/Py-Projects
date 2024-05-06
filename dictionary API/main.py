57620016b7505e50e933a9637590b4ee4ed77c7b
Apurva12S <apurva.srivastava.1812@gmail.com>
2024-05-06 12:23:59 +0530

    fetched the definiton from the csv file

--- dictionary API/main.py
+++ dictionary API/main.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app=Flask("__name__")
+df = pd.read_csv("dictionary.csv")
 
 @app.route("/")
+
+
 
 def home():
     return render_template("home.html")
 
 @app.route("/api/v1/<word>")
 def dictionory(word):
-    meaning = word.upper()    
-    return {"defination":meaning,
-            "word": word}
+    
+
+    definition = df.loc[df['word']== word ]['definition'].squeeze()
+
+        
+    return {"word": word,
+            "definition": definition}
 
 
 if __name__=="__main__":
